5118613f72b66842ba974a8ea40458d234bda99d
joel <joelkalema63@gmail.com>
2022-03-16 22:57:18 +0200

    Added the nav-bar

--- index.js
+++ index.js
@@ -2,45 +2,32 @@ const author = document.querySelector('#author');
 const title = document.querySelector('#title');
 const addBookForm = document.querySelector('#add-book');
 const list = document.querySelector('.list');
-// let books = [];
 
-// addBook(author, title, books) {
-//   const id = books.length + 1;
-//   const bookData = {
-//     id,
-//     author,
-//     title,
-//   };
-//   books.push(bookData);
-// }
+function SetupTabs() {
+  document.querySelectorAll('.tab__button').forEach((button) => {
+    button.addEventListener('click', () => {
+      const sideBar = button.parentElement;
+      const tabsContainer = sideBar.parentElement;
+      const tabNumber = button.dataset.forTab;
+      const tabToActivate = tabsContainer.querySelector(`.tab__content[data-tab ="${tabNumber}"]`);
 
-// removeBook(id, books) {
-//   books.forEach((book) => {
-//     if (book.id === id) {
-//       books.splice(books.indexOf(book), 1);
-//     }
-//   });
-// }
+      sideBar.querySelectorAll('.tab__button').forEach((button) => {
+        button.classList.remove('tab__button--active');
+      });
 
-// displayBooks(list, books) {
-//   list.innerHTML = '';
-//   books.forEach((book) => {
-//     const li = `<li><p>${book.author}</p>
-//                 <h3>${book.title}</h3>
-//                 <button id="${book.id}" class="remove-btn">remove</button></li><hr />`;
-//     list.innerHTML += li;
-//   });
-// }
+      tabsContainer.querySelectorAll('.tab__content').forEach((tab) => {
+        tab.classList.remove('tab__content--active');
+      });
 
-// const setStorage = (data) => {
-//   const formData = JSON.stringify(data);
-//   localStorage.setItem('books', formData);
-// };
+      button.classList.add('tab__button--active');
+      tabToActivate.classList.add('tab__content--active');
+    });
+  });
+}
 
-// const getStorage = (key) => {
-//   const data = JSON.parse(localStorage.getItem(key));
-//   return data;
-// };
+document.addEventListener('DOMContentLoaded', () => {
+  SetupTabs();
+});
 
 class Bookstore {
   constructor() {
