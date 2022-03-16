72adcdf57f654f36efd93b19299f3c32ebd796bc
DanUshindi <danushindi5@gmail.com>
2022-03-16 17:50:29 +0200

    Fix linters

--- index.js
+++ index.js
@@ -2,45 +2,6 @@ const author = document.querySelector('#author');
 const title = document.querySelector('#title');
 const addBookForm = document.querySelector('#add-book');
 const list = document.querySelector('.list');
-// let books = [];
-
-// addBook(author, title, books) {
-//   const id = books.length + 1;
-//   const bookData = {
-//     id,
-//     author,
-//     title,
-//   };
-//   books.push(bookData);
-// }
-
-// removeBook(id, books) {
-//   books.forEach((book) => {
-//     if (book.id === id) {
-//       books.splice(books.indexOf(book), 1);
-//     }
-//   });
-// }
-
-// displayBooks(list, books) {
-//   list.innerHTML = '';
-//   books.forEach((book) => {
-//     const li = `<li><p>${book.author}</p>
-//                 <h3>${book.title}</h3>
-//                 <button id="${book.id}" class="remove-btn">remove</button></li><hr />`;
-//     list.innerHTML += li;
-//   });
-// }
-
-// const setStorage = (data) => {
-//   const formData = JSON.stringify(data);
-//   localStorage.setItem('books', formData);
-// };
-
-// const getStorage = (key) => {
-//   const data = JSON.parse(localStorage.getItem(key));
-//   return data;
-// };
 
 class Bookstore {
   constructor() {
